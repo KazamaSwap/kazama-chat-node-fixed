4c6420da90d746b93fde59ec42194f2e3d571d80
Ivan <davidayewah7@gmail.com>
2023-01-18 02:38:31 +1000

    Update messagesController.js

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,42 +1,55 @@
+// const { ethers } = require("ethers");
+const {
+    verifyTypedData
+} = require("ethers/lib/utils");
 const messageModel = require("../models/messageModel");
 const userModel = require("../models/userModel");
 const types = {
-    AddMessage: [
-        { name: 'sender', type: 'address' },
-        { name: 'message', type: 'string' }
-    ]   
+    AddMessage: [{
+            name: 'sender',
+            type: 'address'
+        },
+        {
+            name: 'message',
+            type: 'string'
+        }
+    ]
 }
 const domain = {
     name: 'chat',
     version: '1',
     // chainId: 1,
     // verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
-  };
+};
 module.exports.addMessage = async (req, res, next) => {
     try {
-        const {sender, message, signature} = req.body;
-      const usermessage = {
-        type: 'EditUsername',
-        sender,
-        message
-      }
-      const signerAddr = await ethers.utils.verifyTypedData(domain, types['EditUsername'], usermessage, signature);
-      if (signerAddr !== address) {
-        return res.json({
-          message: "Not Allowed"
-        });
-      }
+        const {
+            sender,
+            message,
+            signature
+        } = req.body;
+        // const usermessage = {
+        //     type: 'EditUsername',
+        //     sender,
+        //     message
+        // }
+        // const signerAddr = await verifyTypedData(domain, types['EditUsername'], usermessage, signature);
+        // if (signerAddr !== address) {
+        //     return res.json({
+        //         message: "Not Allowed"
+        //     });
+        // }
         const data = await messageModel.create({
-            message:{
+            message: {
                 text: message
             },
             sender: sender,
         });
 
-        if(data) return res.json({
+        if (data) return res.json({
             msg: "Message added successfully!"
         });
-        return res.json({ 
+        return res.json({
             msg: "Failed to add message to DB"
         });
 
@@ -46,13 +59,18 @@ module.exports.addMessage = async (req, res, next) => {
 };
 module.exports.getAllMessage = async (req, res, next) => {
     try {
-        const messages = await messageModel.find().limit(25).sort({ updatedAt: -1 });
+        const messages = await messageModel.find().limit(25).sort({
+            updatedAt: -1
+        });
 
-        const projectMessages = await Promise.all(messages.sort((a,b) => {
+        const projectMessages = await Promise.all(messages.sort((a, b) => {
             return a.updatedAt.getTime() - b.updatedAt.getTime()
-        }).map(async(msg)=>{
-            const user = await userModel.findOne({ '_id' : msg.sender})
-            return{
+        }).map(async (msg) => {
+            console.log(msg.sender)
+            const user = await userModel.findOne({
+                '_id': msg.sender
+            })
+            return {
                 message: msg.message.text,
                 sender: user,
                 updatedAt: msg.updatedAt
@@ -64,4 +82,3 @@ module.exports.getAllMessage = async (req, res, next) => {
         next(error);
     }
 };
-
