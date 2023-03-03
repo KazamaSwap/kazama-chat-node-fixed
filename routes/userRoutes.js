709ce5a558aba7029089a934ea4464a435278130
KazamaSwap <97629820+KazamaSwap@users.noreply.github.com>
2023-03-03 19:17:55 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-const { register, loginOrRegister, setUserName, setAvatar,setKraken, setWhale, setShark, setDolphin, setTurtle, setHolder, setLiquidityProvider, getAllUsers, getUser, isValidName } = require("../controllers/usersController");
+const { register, loginOrRegister, setUserName, setAvatar, setSpacenaut, setKraken, setWhale, setShark, setOrca, setDolphin, setTurtle, setFish, setCrab, setShrimp, setHolder, setLiquidityProvider, getAllUsers, getUser, isValidName } = require("../controllers/usersController");
 const { login } = require("../controllers/usersController");
 const { logOut } = require("../controllers/usersController");
 
@@ -9,11 +9,16 @@ router.post("/connect", loginOrRegister);
 router.post("/getUser", getUser);
 router.post("/isValidName", isValidName);
 router.post("/setAvatar/", setAvatar);
+router.post("/setSpacenaut/:id", setSpacenaut);
 router.post("/setKraken/:id", setKraken);
 router.post("/setWhale/:id", setWhale);
 router.post("/setShark/:id", setShark);
+router.post("/setOrca/:id", setOrca);
 router.post("/setDolphin/:id", setDolphin);
 router.post("/setTurtle/:id", setTurtle);
+router.post("/setFish/:id", setFish);
+router.post("/setCrab/:id", setCrab);
+router.post("/setShrimp/:id", setShrimp);
 router.post("/setHolder/:id", setHolder);
 router.post("/setLiquidityProvider/:id", setLiquidityProvider);
 router.post("/setUserName", setUserName);
